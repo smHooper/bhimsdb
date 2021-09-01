6fb7bd87fbbc21ce1c28631b541f6b40f874690a
smhooper <samhooperstudio@gmail.com>
2021-09-01 09:29:31 -0800

    map query result back to DENA bear fields when data loaded

--- web/js/bhims-custom.js
+++ web/js/bhims-custom.js
@@ -6,7 +6,32 @@ function customizeConfiguration() {
 	$('.dena-bear-field').change(entryForm.onDENABearFieldChange);
 }
 
-/*BHIMSQuery.prototype.fillDENABearFields = function() {
+function customizeQuery() {
 
-}*/
+	/*
+	Map query result back to DENA bear fields
+	*/
+	BHIMSQuery.prototype.setDENABearFields = function() {
+		const selectedResult = bhimsQuery.queryResult[bhimsQuery.selectedID];
+		if (selectedResult) {
+			const bears = selectedResult.bears || [];
+			if (bears.length) {
+				const firstBear = bears[0];
+				for (const el of $('.dena-bear-field')) {
+					const fieldName = el.name;
+					if (fieldName in firstBear) {
+						const value = firstBear[fieldName];
+						$(el).val(value)
+							.toggleClass('default', value == null);
+					}
+				}
+			}
+		}
+
+		// Hide the add-item button
+		$('#bears-accordion').siblings('.add-item-container').addClass('hidden');
+	}
+
+	bhimsQuery.dataLoadedFunctions.push(bhimsQuery.setDENABearFields)
+}
 

54a50642c4bac0604bedd7b7bdf953f10cf13096
Sam Hooper <samuel_hooper@nps.gov>
2023-05-10 10:36:56 -0800

    add anonymized defaults to specific PII fields

--- web/js/query.js
+++ web/js/query.js
@@ -17,7 +17,16 @@ var BHIMSQuery = (function(){
 		this.tableSortColumns = {};
 		this.encounterIDs = [];
 		this.fieldsFull = false;
+		this.anonymizedDefaults = {
+			first_name: 'Anonymoys',
+			last_name: 'Person',
+			phone_number: '555-5555',
+			email_address: 'someone@abc.com' 
+		};
+		this.ignorePIIFields = false;
 		_this = this; // scope hack for event handlers that take over "this"
+
+
 	}
 
 
@@ -177,7 +186,9 @@ var BHIMSQuery = (function(){
 					// For each encounter, create an object that mirrors the FIELD_VALUES object of bhims-entry.js
 					for (const encounter of encounterResult) {
 						
+						
 						this.queryResult[encounter.id] = {...encounter};
+
 						//this.queryResult.encounters[encounter.id] = {...encounter};
 
 						// Configure the list item element for this encounter
@@ -264,6 +275,12 @@ var BHIMSQuery = (function(){
 						const result = $.parseJSON(queryResultString);
 						//this.queryResult[tableName] = {};
 						for (const row of result) {
+							for (const columnName in row) {
+								if ((entryForm.fieldInfo[columnName] || {}).has_pii === 't' && this.anonymizedDefaults[columnName]) {
+									row[columnName] = this.anonymizedDefaults[columnName];
+									this.ignorePIIFields = true;
+								}
+							}
 							const encounterID = row.encounter_id;
 							if (!this.queryResult[encounterID]) {
 								a = 1;
@@ -995,6 +1012,14 @@ var BHIMSQuery = (function(){
 	*/
 	Constructor.prototype.saveEdits = function() {
 		
+		// If the values of any PII fields were automatically set, ignore any changes to them
+		if (this.ignorePIIFields) {
+			const selector = Object.keys(this.anonymizedDefaults)
+				.map(fieldName => `.input-field[name="${fieldName}"]`)
+				.join(', ');
+			$(selector).removeClass('dirty');
+		}
+
 		const $dirtyInputs = $('.input-field.dirty:not(.ignore-on-insert)');
 		if ($dirtyInputs.length === 0) {
 			hideLoadingIndicator();
@@ -2432,6 +2457,8 @@ var BHIMSQuery = (function(){
 				$('#username').text(result[0].username);
 			}
 		});
+
+
 
 		// Data export modal: when the field selection tabs are shown, expand the modal
 		$('#input-select_fields').change(e => {
